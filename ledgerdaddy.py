fb26e6c039288c431d3749d16dd1b7ca98526efb
bhaumiksonii <bhaumik0369@gmail.com>
2025-03-02 20:55:17 +0530

    new changes addaed

--- ledgerdaddy.py
+++ ledgerdaddy.py
@@ -68,6 +68,11 @@ if uploaded_file:
                 # total_money = filtered_df["Balance(INR)"].sum()
                 # st.subheader(f"💰 Total Money: **₹{total_money:,.2f}**")
                 filtered_df = filtered_df.drop("Balance(INR)",axis=1) 
+                filtered_df[['Withdrawals', 'Deposits']] = filtered_df[['Withdrawals', 'Deposits']].replace({',': ''}, regex=True).astype(int)
+
+# Calculate the sum for col5 and col7 and append as a new row
+                new_row = {col: filtered_df[col].sum() if col in ['Withdrawals', 'Deposits'] else '' for col in filtered_df.columns}
+                filtered_df = filtered_df.append(new_row, ignore_index=True)
                 st.dataframe(filtered_df, use_container_width=True,width=400)
 
             else:
