40cc1e28d34be23127b52d2e3dd40c8a7845a232
bhaumiksonii <bhaumik0369@gmail.com>
2025-03-10 22:17:02 +0530

    fix added

--- auth.py
+++ auth.py
@@ -1,18 +1,18 @@
 import streamlit as st
 from streamlit_authenticator import Authenticate
-import os
+import streamlit as st
 import json
-from google.oauth2.service_account import Credentials
 
-# Load credentials from GitHub secrets
-google_credentials_json = os.environ['GOOGLE_CREDENTIALS_JSON']
+# Get Google credentials from Streamlit secrets
+google_credentials_json = st.secrets["GOOGLE_CREDENTIALS_JSON"]
 credentials_info = json.loads(google_credentials_json)
-credentials = Credentials.from_service_account_info(credentials_info)
+
+
 class Auth:
     def __init__(self):
         self.authenticator = Authenticate(
             name='Google Authentication',
-            credentials_file=credentials,  # Add your Google OAuth credentials file here
+            credentials_file=credentials_info,  # Add your Google OAuth credentials file here
         )
 
     def authenticate(self):
